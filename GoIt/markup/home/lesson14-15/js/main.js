242d1abff3befa4191635bebe655b0a6a70c736b
Andrii Sluchik <sloochik89@gmail.com>
2015-12-14 01:27:20 +0200

    lessons14-15_85%

---
+++ GoIt/markup/home/lesson14-15/js/main.js
@@ -0,0 +1,60 @@
+var hour = document.querySelector('.hour');
+var min = document.querySelector('.min');
+var sec = document.querySelector('.sec');
+var milisec = document.querySelector('.milisec');
+var start = document.getElementById('start');
+var stop = document.getElementById('stop');
+var clear = document.getElementById('clear');
+var hourCounter = 0;
+var minCounter =  0;
+var secCounter =  0;
+var miliSecCounter =  0;
+var startT;
+var n;
+
+var app = {
+
+  timer: function(){
+  /*miliSecCounter++;
+    if(miliSecCounter >= 1000){
+      miliSecCounter = 0;
+    }*/
+    secCounter++
+    if(secCounter >= 60){
+       secCounter = 0;
+       minCounter++
+}
+    if(minCounter >= 60){
+      minCounter = 0;
+      hourCounter++
+}
+     if(hourCounter === 24){
+     hourCounter = 0;
+}
+//milisec.innerHTML = '<p>'+('0'+ miliSecCounter).slice(-2) +'</p>';
+sec.innerHTML = '<p>'+('0'+ secCounter).slice(-2) +'</p>';
+min.innerHTML = '<p>'+('0'+ minCounter).slice(-2) +'</p>';
+hour.innerHTML = '<p>'+('0'+ hourCounter).slice(-2) +'</p>';
+},
+  startTimer: function(){
+    startT =  setInterval(app.timer, 1000);
+  }
+};
+
+// start button
+start.onclick = app.startTimer;
+//stop button
+stop.onclick = function() {
+    clearTimeout(startT);
+}
+//clear button
+clear.onclick = function() {
+secCounter = 0;
+minCounter = 0;
+hourCounter = 0;
+//milisec.innerHTML = '<p>'+('0'+ secCounter).slice(-2) +'</p>';
+sec.innerHTML = '<p>'+('0'+ secCounter).slice(-2) +'</p>';
+min.innerHTML = '<p>'+('0'+ minCounter).slice(-2) +'</p>';
+hour.innerHTML = '<p>'+('0'+ hourCounter).slice(-2) +'</p>';
+
+}
