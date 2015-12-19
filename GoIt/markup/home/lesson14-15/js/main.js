cb5071184464883cc471ed83778bb8daadd4e9c5
Andrii Sluchik <sloochik89@gmail.com>
2015-12-19 23:22:42 +0200

    lesson14-15_99%

--- GoIt/markup/home/lesson14-15/js/main.js
+++ GoIt/markup/home/lesson14-15/js/main.js
@@ -2,15 +2,19 @@ var hour = document.querySelector('.hour');
 var min = document.querySelector('.min');
 var sec = document.querySelector('.sec');
 var milisec = document.querySelector('.milisec');
+var split = document.querySelector('.splitbox');
 var start = document.getElementById('start');
 var stop = document.getElementById('stop');
 var clear = document.getElementById('clear');
+var splitBut = document.getElementById('split');
 var hourCounter = 0;
 var minCounter =  0;
 var secCounter =  0;
 var miliSecCounter =  0;
 var startT;
 var n;
+var split_paragraf;
+var count = 0;
 
 var app = {
 
@@ -32,18 +36,18 @@ var app = {
      if(hourCounter >= 24){
      hourCounter = 0;
 }
-milisec.innerHTML = '<p>'+('0'+ miliSecCounter+'0').slice(-3) +'</p>';
+milisec.innerHTML = '<p>'+('0'+ miliSecCounter).slice(-2) +'</p>';
 sec.innerHTML = '<p>'+('0'+ secCounter).slice(-2) +'</p>';
 min.innerHTML = '<p>'+('0'+ minCounter).slice(-2) +'</p>';
 hour.innerHTML = '<p>'+('0'+ hourCounter).slice(-2) +'</p>';
 },
 startTimer: function(){
-    startT =  setInterval(app.timer, 10);
-    start.disabled = true;
-    stop.disabled = false;
+  startT =  setInterval(app.timer, 10);
+  start.disabled = true;
+  stop.disabled = false;
 },
 clearTimer: function(){
-  miliSecCounter = '00';
+  miliSecCounter = 0;
   secCounter = 0;
   minCounter = 0;
   hourCounter = 0;
@@ -51,6 +55,16 @@ clearTimer: function(){
   sec.innerHTML = '<p>'+('0'+ secCounter).slice(-2) +'</p>';
   min.innerHTML = '<p>'+('0'+ minCounter).slice(-2) +'</p>';
   hour.innerHTML = '<p>'+('0'+ hourCounter).slice(-2) +'</p>';
+  split.innerHTML = '';
+  count = 0;
+},
+spliter: function(){
+  count++;
+  var split_n = '<span>lap'+count+'</span> ' + (' 0'+ hourCounter).slice(-2) + ' : ' + ('0'+ minCounter).slice(-2) +' : ' + ('0'+ secCounter).slice(-2) +' : '+ ('0'+ miliSecCounter).slice(-2);
+  split_paragraf = document.createElement('p');
+  split_paragraf.className = 'sp';
+  split.appendChild(split_paragraf);
+  split_paragraf.innerHTML = split_n;
 }
 };
 
@@ -62,5 +76,7 @@ stop.onclick = function() {
     start.disabled = false;
     stop.disabled = true;
 }
-
+//split button
+splitBut.onclick = app.spliter;
+//clear button
 clear.onclick = app.clearTimer;
