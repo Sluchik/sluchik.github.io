c1bec46cdd9426607acc6b922f7bcfc21b8ec8e1
Andrii Sluchik <sloochik89@gmail.com>
2016-01-08 21:50:17 +0200

    lesson19-20-2

--- GoIt/markup/home/lesson19-20/js/main.js
+++ GoIt/markup/home/lesson19-20/js/main.js
@@ -27,6 +27,29 @@
             .jcarouselControl({
                 target: '+=1'
             });
-  $(".chosen-select").chosen({width: "90%"}); 
     });
 })(jQuery);
+
+// select
+  $(function() {
+      $(".chosen-select").chosen({width: "100%"});
+  });
+
+//menu
+$(function() {
+    var $links = $('.menu a');
+    // $links.on('click', function(e){
+    //   var $sabmenu = $(this).siblings('.submenu');
+    //     e.preventDefault();
+    //   console.log($sabmenu);
+    //   $sabmenu.show();
+    // })
+
+    $links.hover(function() {
+     var $sabmenu = $(this).siblings('.submenu');
+      $sabmenu.show();
+ }, function() {
+     var $sabmenu = $(this).siblings('.submenu');
+       $sabmenu.hide();
+ });
+});
