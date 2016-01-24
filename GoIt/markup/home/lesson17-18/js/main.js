2c0eced0e4462d4924099a8bc93ff2905f2cb4da
Andrii Sluchik <sloochik89@gmail.com>
2016-01-24 18:44:17 +0200

    lesson17-18fix

--- GoIt/markup/home/lesson17-18/js/main.js
+++ GoIt/markup/home/lesson17-18/js/main.js
@@ -13,12 +13,12 @@ $("#contaier ul li a").click(function(e){
 });
     $(".tip").hide();
     $("input").hover(function() {
-    $(this).next(".tip").animate({
+    $(this).next(".tip").stop(true, false).animate({
       opacity: "show",
       right: "-100"
     }, "slow");
   }, function() {
-    $(this).next(".tip").animate({opacity: "hide", right: "-120"}, "slow");
+    $(this).next(".tip").stop(true, false).animate({opacity: "hide", right: "-120"}, "slow");
   });
 
   $("button").on('click', function(){
