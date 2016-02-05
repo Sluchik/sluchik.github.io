df6a264f588837cca924de6f0795ef3daf02741a
Andrii Sluchik <sloochik89@gmail.com>
2016-02-05 02:00:28 +0200

    js15-16fix

--- GoIt/markup/home/lessonjs_15-16/js/main.js
+++ GoIt/markup/home/lessonjs_15-16/js/main.js
@@ -1,4 +1,5 @@
 function GoogleCallback(jqueryObj, data){
+
   var $html = $('#template').html();
   var $template = tmpl($html,{
       data: data
@@ -11,24 +12,78 @@ function GoogleCallback(jqueryObj, data){
 
 $(function(){
 
-$('#button').on('click', function(){
-  var ser = 'Ukraine';
-  var $search = $('#searcData');
-  var $url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&rsz=large&q='+ encodeURIComponent($search.val()) +'&callback=GoogleCallback&context=?';
-
+function httprRequest(e){
+  event.preventDefault();
+  $('.present').html("");
+  var $search = encodeURIComponent($('#searcData').val())
+  var $url = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&key=ABQIAAAACKQaiZJrS0bhr9YARgDqUxQBCBLUIYB7IF2WaNrkYqF0tBovNBQFDtM_KNtb3xQxWff2mI5hipc3lg&rsz=large&q='+ $search +'&callback=GoogleCallback&context=?';
   $.ajax({
-    url: $url,
-    dataType: 'jsonp',
-    // metod: 'POST',
-    success: function (data) {
-      console.log('works');
-    }
-  })
+      url: $url,
+      dataType: 'jsonp',
+      // metod: 'POST',
+      success: function (data) {
+        console.log('works');
+      }
+    })
+}
 
-  $('#searcData').keypress(function(e){
-          if(e.which == 13){
-          console.log('hi');
-         }
-     });
-});
+$('#button').on('click', httprRequest);
+$('#target').submit(httprRequest);
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// home work part 2
+/*
+
+$(function(){
+
+function Humen(name, age, sex, he, weight){
+  this.name = name;
+  this.age = age;
+  this.sex = sex;
+  this.he = he;
+  this.weight = weight;
+}
+function Worker (job, salary){
+  Humen.call(this);
+  this.job = job;
+  this.salary = salary;
+}
+
+
+Worker.prototype.work = function(){
+  console.log('I am working');
+}
+
+var andrii = new Worker({
+  name: 'Andrii',
+  age: 18,
+  sex: 'mail',
+  he: 182,
+  weight: 72,
+  job: 'Dev',
+  salary: 2000
+}) ;
+
+console.log(andrii.name);
+});
+*/
