4bf6e97925de9394bcf5bb3fd46fa184667076d6
Andrii Sluchik <sloochik89@gmail.com>
2016-03-21 01:30:32 +0200

    exam5

--- GoIt/markup/home/finalExamJS_HTML/gulpfile.js
+++ GoIt/markup/home/finalExamJS_HTML/gulpfile.js
@@ -7,7 +7,7 @@ var autoprefixer = require('gulp-autoprefixer');
 
 
 gulp.task('css', function () {
-  return gulp.src(['css/normalize.css','css/masonry.css', 'css/commanstyle.css', 'css/fonts.css', 'css/header.css', 'css/sliders.css', 'css/media.css','css/profiles.css', 'css/owl.carousel.css'])
+  return gulp.src(['css/normalize.css','css/masonry.css', 'css/commanstyle.css', 'css/fonts.css', 'css/header.css', 'css/sliders.css', 'css/profiles.css','css/search.css', 'css/footer.css','css/owl.carousel.css','css/media.css'])
   .pipe(autoprefixer({
   			browsers: ['last 2 versions'],
   			cascade: false
---
+++ GoIt/markup/home/finalExamJS_HTML/js/activity.js
@@ -0,0 +1,36 @@
+
+function myMethod(data){
+   $('.holiday_activity').empty();
+   var $html = $('#holiday_activity').html();
+   var $template = tmpl($html,{
+        data: data
+  });
+   $('.holiday_activity').append($template);
+  }
+
+$(function() {
+function httprRequest(e){
+    // e.preventDefault();
+    var search;
+    if($('.search__input').val().length == 0){
+         search = '';
+    } else{
+        search = $('.search__input').val();
+    }
+    var $url = 'http://api.pixplorer.co.uk/image?word='+search +'&amount=6&size=m';
+    $('.search__input').val(" ");
+    $.ajax({
+        url: $url,
+        metod: 'GET',
+        dataType: 'json',
+        success: function (data) {
+        myMethod(data);
+        }
+      })
+ }
+
+ $('#search__button').on('click', httprRequest);
+ $('#target').submit(httprRequest);
+
+httprRequest();
+})
