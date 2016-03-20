1dd7a1a6fcc75ae11144de3a481712fa2f0803f5
Andrii Sluchik <sloochik89@gmail.com>
2016-03-21 01:55:23 +0200

    exam6

--- GoIt/markup/home/finalExamJS_HTML/js/activity.js
+++ GoIt/markup/home/finalExamJS_HTML/js/activity.js
@@ -11,13 +11,21 @@ function myMethod(data){
 $(function() {
 function httprRequest(e){
     // e.preventDefault();
+    var scrinsize;
+    var size;
     var search;
+     scrinsize = screen.width;
+    if(scrinsize <= 460){
+      size = 's';
+    } else{
+        size = 'm';
+    }
     if($('.search__input').val().length == 0){
          search = '';
     } else{
         search = $('.search__input').val();
     }
-    var $url = 'http://api.pixplorer.co.uk/image?word='+search +'&amount=6&size=m';
+    var $url = 'http://api.pixplorer.co.uk/image?word='+search +'&amount=6&size='+size;
     $('.search__input').val(" ");
     $.ajax({
         url: $url,
