2b317145fa81fa7057cab80fec1e2d164dc545f0
Andrii Sluchik <sloochik89@gmail.com>
2016-03-20 02:37:07 +0200

    exam1

--- GoIt/markup/home/finalExamJS_HTML/gulpfile.js
+++ GoIt/markup/home/finalExamJS_HTML/gulpfile.js
@@ -1,13 +1,13 @@
 var gulp = require('gulp');
 var concatCss = require('gulp-concat-css');
 var watch = require('gulp-watch');
-var concatCss = require('gulp-concat-css');
+var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
 
 
 gulp.task('css', function () {
-  return gulp.src(['css/normalize.css', 'css/commanstyle.css', 'css/fonts.css', 'css/header.css', 'css/sliders.css', 'css/media.css','css/profile.css', 'css/owl.carousel.css'])
+  return gulp.src(['css/normalize.css', 'css/commanstyle.css', 'css/fonts.css', 'css/header.css', 'css/sliders.css', 'css/media.css','css/profiles.css', 'css/owl.carousel.css'])
   .pipe(autoprefixer({
   			browsers: ['last 2 versions'],
   			cascade: false
@@ -16,8 +16,15 @@ gulp.task('css', function () {
     .pipe(gulp.dest('css/build/'));
 });
 
+gulp.task('sass', function () {
+     gulp.src('css/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('css/'));
+});
 
-
-gulp.task('watch', function () {
+gulp.task('watch',['css'], function () {
+   gulp.watch('css/sass/*.scss', ['sass'])
    gulp.watch('css/*.css', ['css'])
-})
+});
+
+gulp.task('default', ['watch']);
---
+++ GoIt/markup/home/finalExamJS_HTML/js/profiles.js
@@ -0,0 +1,40 @@
+
+$(function() {
+var profiles =[
+{
+  avatar:'img/ava1.jpg',
+  name: 'Bradley Hunter',
+  info: 'Based in Chicago. I love playing tennis and loud music.',
+  status:'img/tv.png',
+},
+{
+  avatar:'img/ava2.jpg',
+  name: 'Heather Walker',
+  info: 'Im a happy person that loves cats and climbing on mountains.',
+  status:'img/chem.png',
+},
+{
+  avatar:'img/ava3.jpg',
+  name: 'Lucas Marsha',
+  info: 'I get my inspiration from nature and objects around me. I have a passion to colours, typography and skateboards.',
+  status:'img/cof.png',
+},
+{
+  avatar:'img/ava4.jpg',
+  name: 'Bradley Hunter',
+  info: 'Based in Chicago. I love playing tennis and loud music.',
+  status:'img/trev.png',
+ }
+];
+
+
+
+var $html = $('#person').html(); //.html() записує повнічтю html вміст
+var $template = tmpl($html,{
+    data: profiles
+});
+
+//tmpl - виклик шаблону
+//в шаблон потрібно передавати обєет а не масив !масив не працює
+$('.person__box').append($template);
+})
