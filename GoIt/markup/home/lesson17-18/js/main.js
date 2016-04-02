cc6ed161aa75a7b5f7e12380cddcb0fc5f54e1d1
Andrii Sluchik <sloochik89@gmail.com>
2016-04-02 18:38:22 +0300

    paintings_by_Piet_Mondrian

--- GoIt/markup/home/lesson17-18/js/main.js
+++ GoIt/markup/home/lesson17-18/js/main.js
@@ -1,28 +1,27 @@
-
 $(function() {
 $(".tabContents").hide();
 $(".tabContents:first").show();
 
 $("#contaier ul li a").click(function(e){
      var activeTab = $(this).attr("href");
-    $("#contaier ul li a").removeClass("active");
-     $(this).addClass("active");
+    $("#contaier ul li a").removeClass("tab_active");
+     $(this).addClass("tab_active");
      $(".tabContents").hide();
      $(activeTab).fadeIn(100);
     e.preventDefault();
 });
-    $(".tip").hide();
-    $("input").hover(function() {
-    $(this).next(".tip").stop(true, false).animate({
-      opacity: "show",
-      right: "-100"
-    }, "slow");
-  }, function() {
-    $(this).next(".tip").stop(true, false).animate({opacity: "hide", right: "-120"}, "slow");
-  });
-
-  $("button").on('click', function(){
-      $(".tip").show();
-  })
+  //   $(".tip").hide();
+  //   $("input").hover(function() {
+  //   $(this).next(".tip").stop(true, false).animate({
+  //     opacity: "show",
+  //     right: "-100"
+  //   }, "slow");
+  // }, function() {
+  //   $(this).next(".tip").stop(true, false).animate({opacity: "hide", right: "-120"}, "slow");
+  // });
+  //
+  // $("button").on('click', function(){
+  //     $(".tip").show();
+  // })
 
 });
