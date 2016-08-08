99543948e32b2f4cc295919a5c72466cfb3dce97
Andrii Sluchik <sloochik89@gmail.com>
2016-08-08 15:03:47 +0300

    Update main.js

--- GoIt/markup/home/lesson17-18/js/main.js
+++ GoIt/markup/home/lesson17-18/js/main.js
@@ -10,18 +10,18 @@ $("#contaier ul li a").click(function(e){
      $(activeTab).fadeIn(100);
     e.preventDefault();
 });
-  //   $(".tip").hide();
-  //   $("input").hover(function() {
-  //   $(this).next(".tip").stop(true, false).animate({
-  //     opacity: "show",
-  //     right: "-100"
-  //   }, "slow");
-  // }, function() {
-  //   $(this).next(".tip").stop(true, false).animate({opacity: "hide", right: "-120"}, "slow");
-  // });
-  //
-  // $("button").on('click', function(){
-  //     $(".tip").show();
-  // })
+     $(".tip").hide();
+     $("input").hover(function() {
+     $(this).next(".tip").stop(true, false).animate({
+       opacity: "show",
+       right: "-100"
+     }, "slow");
+   }, function() {
+     $(this).next(".tip").stop(true, false).animate({opacity: "hide", right: "-120"}, "slow");
+   });
+  
+   $("button").on('click', function(){
+       $(".tip").show();
+   })
 
 });
